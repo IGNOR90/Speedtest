b281710bf0335b03f7ccbdff8247044f55dee30a
CreatorGTO <42001707+IGNOR90@users.noreply.github.com>
2023-10-14 17:01:01 +0500

    Обновление и оптимизация кода, перенос основного конфига на сервер.

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import time
 import json
 import requests
-from exbillparser import ExBillParser, CONFIG
+import whatismyip
 import spt as Tester
-from config import *
 
 
 # ip = requests.get("https://api.ipify.org/?format=json").json()
 file = open('serverList.json')
 serverList = json.load(file)
+
+_config = {
+    'timeout': 3600,
+    'api_url': 'https://panel.spi.uz/speedtest/api/'
+}
 
 
 class HttpResponse:
@@ -17,7 +21,7 @@ class HttpResponse:
 
 def send_test_data(obj):
     obj_ = json.dumps(obj)
-    req = requests.post(url=config['server_url'], data=obj_)
+    req = requests.post(url=_config['api_url'], data=obj_)
 
 
 def find(serversList, server_id):
@@ -27,50 +31,71 @@ def find(serversList, server_id):
     return None
 
 
-def change_tariff_name(tariff_name_id):
-    parser = ExBillParser()
-    getPage = parser.request('GET', url=CONFIG.get('client-url'))
-    data = parser.formParse(getPage.content)
-    data.update({'current_tariff_name_id': tariff_name_id})
-    print(data)
-    return parser.request('POST', url=CONFIG.get('client-url'), data=data)
-
-
 tariff_names = {
+    564: '100',
     563: '10',
-    564: '100'
 }
 
 
-def spTest(tariff_name_id):
-    change_tariff_name(tariff_name_id)
+def config_get():
+    obj = {
+        'client_ip': whatismyip.whatismyip()
+    }
+    response = requests.post(url='https://panel.spi.uz/speedtest/api/', json=obj)
+    client_data = response.text
+    json_object = json.loads(client_data)
+
+    return json_object
+
+
+def change_tariff(bill_user_id, tarif_id):
+    obj = {}
+    obj['bill_user_id'] = bill_user_id
+    obj['tarif_id'] = tarif_id
+    print(obj)
+    req = requests.post(url='https://panel.spi.uz/speedtest/api/change_tariff', json=obj)
+    print(req.text)
+    return ''
+
+
+def start_test():
+    data = config_get()
+    print(data)
+    tarif_data = {}
+
+    for a in data['tarif_lists']:
+        tarif_data[a['tarif_id']] = a['tarif_name']
+
+    for tarif_ in tarif_data:
+        tarif_name = tarif_data[tarif_]
+        print(tarif_, '---', tarif_name)
+
+        oob = change_tariff(data['bill_user_id'], tarif_)
+        # obj = speedtest()
+        # send_test_data(obj)
+        time.sleep(60)
+
+
+def speedtest():
     st = Tester.Speedtest()
     setServer = find(serverList, '6386')
     st.get_best_server([setServer])
     st.set_best(setServer)
-     
     obj = {
-        'client': config['client'],
         'server_ping': st.results.ping,
         'server_download': round(st.download() / 1000 / 1000, 1),
         'server_upload': round(st.upload() / 1000 / 1000, 1),
-        'tariff_name': tariff_names.get(tariff_name_id),
-        'client_ip': config['client_ip']
+        'client_ip': _config['client_ip']
     }
 
-    send_test_data(obj)
-    time.sleep(10)
+    return obj
 
 
 def run(sleep):
     while True:
-        try:
-            spTest(563)
-            spTest(564)
-        except:
-            print("ERROR spTest")
+        start_test()
         time.sleep(sleep)
 
 
 if __name__ == "__main__":
-    run(config['sleep'])
+    run(_config['timeout'])
