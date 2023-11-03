48e33eb32fba4e59fa7640dbc9d482b2bedf8a24
CreatorGTO <42001707+IGNOR90@users.noreply.github.com>
2023-11-03 15:01:51 +0500

    Обновление и оптимизация кода, перенос основного конфига на сервер.

--- main.py
+++ main.py
@@ -78,33 +78,3 @@ if __name__ == "__main__":
     # run(_config['timeout'])
     run(_config['timeout'])
 
-{37382: {'type': 'result', 'timestamp': '2023-11-03T09:58:04Z', 'ping': {'jitter': 0.335, 'latency': 1.637},
-         'download': {'bandwidth': 11321197, 'bytes': 43176464, 'elapsed': 3810},
-         'upload': {'bandwidth': 11765193, 'bytes': 42433640, 'elapsed': 3607}, 'isp': 'SPECTR IT',
-         'interface': {'internalIp': '10.34.61.110', 'name': 'enp3s0', 'macAddr': 'B4:2E:99:59:5D:9F', 'isVpn': False,
-                       'externalIp': '93.170.220.67'},
-         'server': {'id': 37382, 'host': 'speedtest.st.uz', 'port': 8080, 'name': 'Sharq Telekom',
-                    'location': 'Tashkent', 'country': 'Uzbekistan', 'ip': '217.29.116.215'},
-         'result': {'id': '1fef7e7c-6487-44ff-86a9-4c6135b02321',
-                    'url': 'https://www.speedtest.net/result/c/1fef7e7c-6487-44ff-86a9-4c6135b02321',
-                    'persisted': True}, 'tariff_name': 'tariff_name'}}
-{37382: {'type': 'result', 'timestamp': '2023-11-03T09:58:04Z', 'ping': {'jitter': 0.335, 'latency': 1.637},
-         'download': {'bandwidth': 11321197, 'bytes': 43176464, 'elapsed': 3810},
-         'upload': {'bandwidth': 11765193, 'bytes': 42433640, 'elapsed': 3607}, 'isp': 'SPECTR IT',
-         'interface': {'internalIp': '10.34.61.110', 'name': 'enp3s0', 'macAddr': 'B4:2E:99:59:5D:9F', 'isVpn': False,
-                       'externalIp': '93.170.220.67'},
-         'server': {'id': 37382, 'host': 'speedtest.st.uz', 'port': 8080, 'name': 'Sharq Telekom',
-                    'location': 'Tashkent', 'country': 'Uzbekistan', 'ip': '217.29.116.215'},
-         'result': {'id': '1fef7e7c-6487-44ff-86a9-4c6135b02321',
-                    'url': 'https://www.speedtest.net/result/c/1fef7e7c-6487-44ff-86a9-4c6135b02321',
-                    'persisted': True}, 'tariff_name': 'tariff_name'},
- 46774: {'type': 'result', 'timestamp': '2023-11-03T09:58:14Z', 'ping': {'jitter': 0.128, 'latency': 1.277},
-         'download': {'bandwidth': 11776572, 'bytes': 42585680, 'elapsed': 3616},
-         'upload': {'bandwidth': 11766141, 'bytes': 42449568, 'elapsed': 3608}, 'packetLoss': 0, 'isp': 'SPECTR IT',
-         'interface': {'internalIp': '10.34.61.110', 'name': 'enp3s0', 'macAddr': 'B4:2E:99:59:5D:9F', 'isVpn': False,
-                       'externalIp': '93.170.220.67'},
-         'server': {'id': 46774, 'host': 'speedtest2.uztelecom.uz', 'port': 8080, 'name': 'UZTELECOM',
-                    'location': 'Tashkent', 'country': 'Uzbekistan', 'ip': '195.69.189.215'},
-         'result': {'id': '43317519-3e9f-4748-ab4b-dae109de7226',
-                    'url': 'https://www.speedtest.net/result/c/43317519-3e9f-4748-ab4b-dae109de7226',
-                    'persisted': True}, 'tariff_name': 'tariff_name'}}
